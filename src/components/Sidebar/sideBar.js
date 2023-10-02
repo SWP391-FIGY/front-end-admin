41a5e7718c815b73fcdcca281aa35305ebc3674d
Lê Đạt <datlt.mdc@gmail.com>
2023-10-03 00:51:28 +0700

    Login page and Sidebar

---
+++ src/components/Sidebar/sideBar.js
@@ -0,0 +1,96 @@
+'use client'
+
+const { Sidebar } = require("flowbite-react")
+import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from 'react-icons/hi';
+
+
+const PageSidebar = () => {
+
+  const SidebarData = [
+  {
+    title: "Dashboard",
+    icon: <HiChartPie/>
+  },
+  {
+    title: "Kanban",
+    icon: <HiViewBoards/>
+  },
+  {
+    title: "Dashboard",
+    icon: <HiChartPie/>
+  },
+  {
+    title: "Kanban",
+    icon: <HiViewBoards/>
+  },
+]
+  return (
+    <Sidebar aria-label="Default sidebar example">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Sidebar.Item
+            href="#"
+            icon={HiChartPie}
+          >
+            <p>
+              Dashboard
+            </p>
+          </Sidebar.Item>
+          <Sidebar.Item
+            href="#"
+            icon={HiViewBoards}
+            label="Pro"
+            labelColor="dark"
+          >
+            <p>
+              Kanban
+            </p>
+          </Sidebar.Item>
+          <Sidebar.Item
+            href="#"
+            icon={HiInbox}
+            label="3"
+          >
+            <p>
+              Inbox
+            </p>
+          </Sidebar.Item>
+          <Sidebar.Item
+            href="#"
+            icon={HiUser}
+          >
+            <p>
+              Users
+            </p>
+          </Sidebar.Item>
+          <Sidebar.Item
+            href="#"
+            icon={HiShoppingBag}
+          >
+            <p>
+              Products
+            </p>
+          </Sidebar.Item>
+          <Sidebar.Item
+            href="#"
+            icon={HiArrowSmRight}
+          >
+            <p>
+              Sign In
+            </p>
+          </Sidebar.Item>
+          <Sidebar.Item
+            href="#"
+            icon={HiTable}
+          >
+            <p>
+              Sign Up
+            </p>
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  )
+}
+
+export default PageSidebar
