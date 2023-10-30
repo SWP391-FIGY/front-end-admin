344cd965517f62feca6fb16a67ba212ce24239c9
KhangGrey <legendofgrey123@gmail.com>
2023-10-30 13:59:35 +0700

    commit

--- src/app/foods/index/foodInfo.js
+++ src/app/foods/index/foodInfo.js
@@ -3,40 +3,64 @@ export const foodInfo = [
 		name: 'orange',
         nutritionalIngredients: 'vitamin C',
 		storageConditions: 'keep in cool places',
+		unit: 10,
+		standardPrice: 100,
+		total: 15
 	},
     {
 		name: 'orange',
         nutritionalIngredients: 'vitamin C',
 		storageConditions: 'keep in cool places',
+		unit: 10,
+		standardPrice: 100,
+		total: 15
 	},
     {
 		name: 'orange',
         nutritionalIngredients: 'vitamin C',
 		storageConditions: 'keep in cool places',
+		unit: 10,
+		standardPrice: 100,
+		total: 15
 	},
     {
 		name: 'orange',
         nutritionalIngredients: 'vitamin C',
 		storageConditions: 'keep in cool places',
+		unit: 10,
+		standardPrice: 100,
+		total: 15
 	},
     {
 		name: 'orange',
         nutritionalIngredients: 'vitamin C',
 		storageConditions: 'keep in cool places',
+		unit: 10,
+		standardPrice: 100,
+		total: 15
 	},
     {
 		name: 'orange',
         nutritionalIngredients: 'vitamin C',
 		storageConditions: 'keep in cool places',
+		unit: 10,
+		standardPrice: 100,
+		total: 15
 	},
     {
 		name: 'orange',
         nutritionalIngredients: 'vitamin C',
 		storageConditions: 'keep in cool places',
+		unit: 10,
+		standardPrice: 100,
+		total: 15
 	},
     {
 		name: 'orange',
         nutritionalIngredients: 'vitamin C',
 		storageConditions: 'keep in cool places',
+		unit: 10,
+		standardPrice: 100,
+		total: 15
 	},
 ]
