23eedce504c191e45395f1fbb65907cd82c88da4
KhangGrey <legendofgrey123@gmail.com>
2023-11-04 12:35:11 +0700

    fix user Edit and added crud for Food

--- src/app/foods/create/page.jsx
+++ src/app/foods/create/page.jsx
@@ -1,11 +1,23 @@
-'use client'
-import { Button, Label, Select, TextInput, Datepicker, Spinner } from "flowbite-react"
-import { useFormik } from "formik"
-import * as Yup from 'yup'
-import { usePathname } from "next/navigation"
-import { useState } from "react"
-import Link from "next/link"
-import { HiOutlineArrowSmallLeft } from "react-icons/hi2"
+"use client";
+import {
+  Button,
+  Label,
+  Select,
+  TextInput,
+  Datepicker,
+  Spinner,
+} from "flowbite-react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { HiOutlineArrowSmallLeft } from "react-icons/hi2";
+import { message } from "antd";
+import { HiPlus } from "react-icons/hi";
+import moment from "moment/moment";
+import axios from "axios";
+import { API } from "@/constants";
 
 const { default: PageLayout } = require("@/layout/pageLayout")
 
@@ -16,30 +28,41 @@ const FoodCreatePage = () => {
     initialValues: {
       name: '',
       nutritionalIngredients: '',
-      storageConditions: 'keep in cool places',
+      storageCondition: 'keep in cool places',
+      unit: 'kg',
+      standardPrice: '',
+      safetyThreshold: '',
     },
     validationSchema: Yup.object({
       name: Yup.string().required('Required'),
       nutritionalIngredients: Yup.string().required('Required'),
-      storageConditions: Yup.string().required('Required'),
+      storageCondition: Yup.string().required('Required'),
+      unit: Yup.string().required('Required'),
+      standardPrice: Yup.number().required('Required'),
+      safetyThreshold: Yup.number().required('Required'),
     }),
-    onSubmit: values => {
-      setSpinner(true)
+    onSubmit: (values) => {
+      setSpinner(true);
       const payloadData = {
         data: values,
-      }
-      console.log("Submitted");
-      console.log(values);
+      };
+      console.log(payloadData.data);
       axios
-        .post(`${API}/feedbacks`, payloadData)
-        .then(response => {
-          setSpinner(false)
-          formik.resetForm()
+        .post(`${API}/food`, payloadData.data)
+        .then((response) => {
+          setSpinner(false);
+          formik.resetForm();
+          
+          router.push("/foods/index")
         })
-        .catch(error => {
-          setSpinner(false)
-          console.log('An error occurred:', error.response)
+        .then((response) => {
+          message.success("Add new food success");
         })
+        .catch((error) => {
+          message.error("An error occurred");
+          setSpinner(false);
+          console.log("An error occurred:", error.response);
+        });
     },
   })
 
@@ -94,26 +117,90 @@ const FoodCreatePage = () => {
           </div>
           <div className="flex flex-col gap-2">
             <Label
-              htmlFor="storageConditions"
-              value="Storage conditions"
+              htmlFor="storageCondition"
+              value="Storage condition"
             />
             <Select
-              id="storageConditions"
+              id="storageCondition"
 
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              value={formik.values.storageConditions}
+              value={formik.values.storageCondition}
             >
               <option>keep in cool places</option>
               <option>refrigerated</option>
 
             </Select>
-            {formik.touched.storageConditions && formik.errors.storageConditions ? (
+            {formik.touched.storageCondition && formik.errors.storageCondition ? (
               <div className='text-xs text-red-600 dark:text-red-400'>
-                {formik.errors.storageConditions}
+                {formik.errors.storageCondition}
               </div>
             ) : null}
           </div>
+
+          <div className="flex flex-col gap-2">
+            <Label
+              htmlFor="unit"
+              value="Unit"
+            />
+            <Select
+              id="unit"
+
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.unit}
+            >
+              <option>kg</option>
+              <option>each</option>
+
+            </Select>
+            {formik.touched.unit && formik.errors.unit ? (
+              <div className='text-xs text-red-600 dark:text-red-400'>
+                {formik.errors.unit}
+              </div>
+            ) : null}
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Label
+              htmlFor="standardPrice"
+              value="Standard price"
+            />
+            <TextInput
+              id="standardPrice"
+
+              type="text"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.standardPrice}
+            />
+            {formik.touched.standardPrice && formik.errors.standardPrice ? (
+              <div className='text-xs text-red-600 dark:text-red-400'>
+                {formik.errors.standardPrice}
+              </div>
+            ) : null}
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Label
+              htmlFor="safetyThreshold"
+              value="Safety threshold"
+            />
+            <TextInput
+              id="safetyThreshold"
+
+              type="text"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.safetyThreshold}
+            />
+            {formik.touched.safetyThreshold && formik.errors.safetyThreshold ? (
+              <div className='text-xs text-red-600 dark:text-red-400'>
+                {formik.errors.safetyThreshold}
+              </div>
+            ) : null}
+          </div>
+          
           <Button type="submit">
             {spinner ? (
               <div className='flex justify-center items-center gap-4'>
