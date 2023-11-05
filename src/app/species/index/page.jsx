f615e8c19af378bcb1c97b37c08e1d9df516dc6b
KhangGrey <legendofgrey123@gmail.com>
2023-11-05 23:23:14 +0700

    added Axios for Index and Create Species

--- src/app/species/index/page.jsx
+++ src/app/species/index/page.jsx
@@ -1,41 +1,34 @@
-'use client'
-import SpeciesList from "./speciesList"
-import PageLayout from '@/layout/pageLayout'
-import { HiPlus } from 'react-icons/hi';
+"use client";
+import SpeciesList from "./speciesList";
+import PageLayout from "@/layout/pageLayout";
+import { HiPlus } from "react-icons/hi";
 import { useRouter } from "next/navigation";
 import { Button } from "flowbite-react";
-
+import Link from "next/link";
 
 const SpeciesListPage = () => {
-	const router = useRouter()
+  const router = useRouter();
 
-  const handleAddClick = (e) => {
-    e.preventDefault()
-    router.push('/species/create')
-  }
-	return (
-		<PageLayout>
-			<div className='w-full p-10 flex flex-col gap-4 h-[100vh] overflow-y-scroll'>
-				<div className='dashboard-content-header flex flex-row justify-between'>
-					<h2 className='text-3xl font-bold'>Species List</h2>
-					<div onClick={handleAddClick}>
-						<Button>
-							<div className='flex flex-row justify-center gap-4'>
-								<div className="my-auto">
-									<HiPlus />
-								</div>
-								<p>
-									Add new species
-								</p>
-							</div>
-						</Button>
-					</div>
-				</div>
-				<SpeciesList />
-			</div>
+  return (
+    <PageLayout>
+      <div className="w-full p-10 flex flex-col gap-4 h-[100vh] overflow-y-scroll">
+        <div className="flex flex-row justify-between">
+          <h2 className="text-3xl font-bold">Species List</h2>
+          <Link href={"/species/create"}>
+            <Button>
+              <div className="flex flex-row justify-center gap-4">
+                <div className="my-auto">
+                  <HiPlus />
+                </div>
+                <p>Add new species</p>
+              </div>
+            </Button>
+          </Link>
+        </div>
+        <SpeciesList />
+      </div>
+    </PageLayout>
+  );
+};
 
-		</PageLayout>
-	)
-}
-
-export default SpeciesListPage
+export default SpeciesListPage;
